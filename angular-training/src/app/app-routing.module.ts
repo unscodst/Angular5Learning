361b5899531d904443e07739b260e85b035f6f89
unscodst <spikester@sbcglobal.net>
2018-09-13 22:21:39 -0700

    Adding Animations
    Animations now work on page load, when adding an item to the list and removing items from a list.

--- angular-training/src/app/app-routing.module.ts
+++ angular-training/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
 const routes: Routes = [];
 
 @NgModule({
--- angular-training/src/app/home/home.component.ts
+++ angular-training/src/app/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import {trigger,style,transition,animate,keyframes,query,stagger} from '@angular/animations';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('goals', [
+      transition('* => *', [
+        query(':enter', style({opacity:0}), {optional: true}),
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-50%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(0)', offset: .5}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+          ]))]),
+          {optional: true}),
+          query(':leave', stagger('300ms', [
+            animate('.6s ease-in', keyframes([
+              style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+              style({opacity: .5, transform: 'translateY(10px)', offset: .5}),
+              style({opacity: 0, transform: 'translateY(-50%)', offset: 1})
+            ]))]),
+            {optional: true})
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
   itemCount: number;
   btnText: string = 'Add an item';
   goalText: string = 'My first life goal';
-  goals = [];
+  goals = ['My first life goal','I want to climb a mountain','Go ice skating'];
 
   constructor() { }
 
@@ -21,5 +42,8 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+  }
+  removeItem(i) {
+    this.goals.splice(i, 1);
   }
 }

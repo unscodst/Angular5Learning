29b2073f41b59025ba6ff19b8b2e80c187a72e6d
unscodst <spikester@sbcglobal.net>
2018-09-13 21:16:55 -0700

    Adding Forms
    Importing forms module

--- angular-training/src/app/home/home.component.ts
+++ angular-training/src/app/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  itemCount: number = 4;
+  btnText: string = 'Add an item';
   constructor() { }
 
   ngOnInit() {
